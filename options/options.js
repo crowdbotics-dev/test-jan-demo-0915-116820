4a4ce969303d2885a53c9abe809f8ebe5e4cadec
Jan Acuna <jan.acuna@crowdbotics.com>
2023-09-15 07:57:34 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "test-jan-demo-0915-116820",
   url: "https://test-jan-demo-0915-116820.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- store/testjandemoAPI/api.js
+++ store/testjandemoAPI/api.js
@@ -1,52 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const testjandemoAPI = axios.create({
   baseURL: "https://test-jan-demo-0915-116820.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return testjandemoAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return testjandemoAPI.post(`/api/v1/login/`, payload)
+  return testjandemoAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return testjandemoAPI.post(`/api/v1/signup/`, payload)
+  return testjandemoAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return testjandemoAPI.post(`/rest-auth/login/`, payload)
+  return testjandemoAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return testjandemoAPI.get(`/rest-auth/logout/`)
+  return testjandemoAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return testjandemoAPI.post(`/rest-auth/logout/`)
+  return testjandemoAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return testjandemoAPI.post(`/rest-auth/password/change/`, payload)
+  return testjandemoAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return testjandemoAPI.post(`/rest-auth/password/reset/`, payload)
+  return testjandemoAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return testjandemoAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return testjandemoAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return testjandemoAPI.post(`/rest-auth/registration/`, payload)
+  return testjandemoAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return testjandemoAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return testjandemoAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return testjandemoAPI.get(`/rest-auth/user/`)
+  return testjandemoAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return testjandemoAPI.put(`/rest-auth/user/`, payload)
+  return testjandemoAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return testjandemoAPI.patch(`/rest-auth/user/`, payload)
+  return testjandemoAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -62,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
