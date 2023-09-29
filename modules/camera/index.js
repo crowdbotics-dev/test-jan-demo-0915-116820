68dc606d3828c9221905361a45539912cdabb44d
Crowdbotics <engineering@crowdbotics.com>
2023-09-29 15:17:03 +0000

    add component Camera

--- modules/camera/index.js
+++ modules/camera/index.js
@@ -1,8 +1,7 @@
-import { StyleSheet } from "react-native";
 import React, { useRef, useContext, useEffect, useState } from 'react';
-import { Text, View, TouchableOpacity, FlatList, ImageBackground } from 'react-native';
-import ActionSheet from 'react-native-actionsheet';
-import { pickFromCamera, pickFromGallery, uploadImage } from './utils';
+import { Text, View, TouchableOpacity, FlatList, ImageBackground, } from 'react-native';
+import ActionSheet from 'react-native-actionsheet'
+import { pickFromCamera, pickFromGallery, uploadImage } from './utils'
 import { OptionsContext, GlobalOptionsContext } from "@options";
 
 const Camera = () => {
@@ -13,64 +12,71 @@ const Camera = () => {
   const [isLoading, setLoading] = useState(false);
   const ImagePickerOptions = ['Take Photo', 'Choose from Gallery', 'Cancel'];
   const [data, setData] = useState([]);
-  const {
-    styles,
-    buttonText
-  } = options;
+
+  const { styles, buttonText } = options;
 
   const fetch_images = () => {
-    fetch(`${gOptions.url}/modules/camera/photos/user/`).then(response => response.json()).then(json => setData(json)).catch(error => console.log(error)).finally(() => setLoading(false));
-  };
+    fetch(`${gOptions.url}/modules/camera/photos/user/`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }
 
   useEffect(() => {
-    fetch_images();
+    fetch_images()
   }, []);
 
-  const renderItem = ({
-    item
-  }) => <TouchableOpacity>
-      <ImageBackground source={{
-      uri: `${gOptions.url}/${item.image}`
-    }} style={styles.image}>
+  const renderItem = ({ item }) => (
+    <TouchableOpacity>
+      <ImageBackground source={{ uri: `${gOptions.url}/${item.image}` }} style={styles.image}>
       </ImageBackground>
-    </TouchableOpacity>;
+    </TouchableOpacity >
+  )
 
-  return <View style={_styles.gyagtmpQ}>
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}>
 
 
-      <FlatList data={data} keyExtractor={item => item.id} renderItem={renderItem} />
-      <ActionSheet ref={actionSheet} title={'Select Image'} options={ImagePickerOptions} cancelButtonIndex={2} onPress={async index => {
-      let res;
-
-      switch (index) {
-        case 0:
-          res = await pickFromCamera();
-          res && uploadImage(res, gOptions).then(() => {
-            fetch_images();
-          });
-          break;
-
-        case 1:
-          res = await pickFromGallery();
-          res && uploadImage(res, gOptions).then(() => {
-            fetch_images();
-          });
-          break;
-      }
-    }} />
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
+      <ActionSheet
+        ref={actionSheet}
+        title={'Select Image'}
+        options={ImagePickerOptions}
+        cancelButtonIndex={2}
+        onPress={async (index) => {
+          let res;
+          switch (index) {
+            case 0:
+              res = await pickFromCamera();
+              res && uploadImage(res, gOptions).then(() => {
+                fetch_images()
+              })
+              break;
+            case 1:
+              res = await pickFromGallery();
+              res && uploadImage(res, gOptions).then(() => {
+                fetch_images()
+              })
+              break;
+          }
+        }}
+      />
       <TouchableOpacity onPress={() => actionSheet.current.show()} style={styles.photoBtn}>
         <Text style={styles.photoBtnTxt}>{buttonText}</Text>
       </TouchableOpacity>
-    </View>;
+    </View>
+  );
 };
 
 export default {
   title: "Camera",
   navigator: Camera
-};
-
-const _styles = StyleSheet.create({
-  gyagtmpQ: {
-    flex: 1
-  }
-});
+}
--- modules/camera/utils.js
+++ modules/camera/utils.js
@@ -1,4 +1,7 @@
-import { Alert, Platform } from 'react-native';
+import {
+  Alert,
+  Platform
+} from 'react-native';
 import * as Permissions from 'react-native-permissions';
 import ImagePicker from 'react-native-image-crop-picker';
 import axios from "axios";
@@ -6,17 +9,14 @@ import axios from "axios";
 async function askPermission(permission) {
   try {
     const status = await Permissions.check(permission);
-
     if (status !== Permissions.RESULTS.GRANTED) {
       //if not already granted then ask
       const status = await Permissions.request(permission);
-
       if (status !== Permissions.RESULTS.GRANTED) {
         //user denied on ask
         return false;
       }
     }
-
     return true;
   } catch (err) {
     console.log('askPermission err', err, ' for permission', permission);
@@ -28,39 +28,44 @@ export async function getCameraGalleryPermissions() {
   //need both permisisons for camera, so ask both on galery and camera
   const {
     PERMISSIONS
-  } = Permissions;
+  } = Permissions
   let permission = Platform.select({
     android: PERMISSIONS.ANDROID.CAMERA,
-    ios: PERMISSIONS.IOS.CAMERA
-  });
+    ios: PERMISSIONS.IOS.CAMERA,
+  })
+
   let cameraPermissions = await askPermission(permission);
   permission = Platform.select({
     android: PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
-    ios: PERMISSIONS.IOS.PHOTO_LIBRARY
-  });
+    ios: PERMISSIONS.IOS.PHOTO_LIBRARY,
+  })
   let storagePermissions = await askPermission(permission);
-  return cameraPermissions && storagePermissions;
+  return cameraPermissions && storagePermissions
 }
+
+
 
 function permissionsAlert() {
-  Alert.alert('Permissions Required', 'App requires Camera & Photos access to function properly. Please go to settings to enable manually.', [{
-    text: 'Cancel',
-    onPress: () => {
-      console.log('Cancel Pressed');
+  Alert.alert(
+    'Permissions Required',
+    'App requires Camera & Photos access to function properly. Please go to settings to enable manually.',
+    [{
+      text: 'Cancel',
+      onPress: () => { console.log('Cancel Pressed') },
+      style: 'cancel'
     },
-    style: 'cancel'
-  }, {
-    text: 'Settings',
-    onPress: () => {
-      Permissions.openSettings().catch(() => console.log('cannot open settings'));
-    }
-  }]);
-}
+    {
+      text: 'Settings',
+      onPress: () => {
+        Permissions.openSettings().catch(() => console.log('cannot open settings'))
+      }
+    },
+    ]
+  )
+};
 
-;
 export const pickFromGallery = async () => {
   let havePermission = await getCameraGalleryPermissions();
-
   if (!havePermission) {
     permissionsAlert();
     return false;
@@ -73,16 +78,16 @@ export const pickFromGallery = async () => {
         mediaType: 'photo',
         includeBase64: true
       });
-      return res;
+      return res
     } catch (err) {
-      console.log('pickFromGallery err', err);
+      console.log('pickFromGallery err', err)
       return false;
     }
   }
 };
+
 export const pickFromCamera = async () => {
   let havePermission = await getCameraGalleryPermissions();
-
   if (!havePermission) {
     permissionsAlert();
     return false;
@@ -97,29 +102,32 @@ export const pickFromCamera = async () => {
       });
       return res;
     } catch (err) {
-      console.log('pickFromCamera err', err);
+      console.log('pickFromCamera err', err)
       return false;
     }
   }
 };
+
 const APP_PLATFORM = "Mobile";
+
 export const request = axios.create({
   headers: {
     app_platform: APP_PLATFORM,
-    app_version: 1
+    app_version: 1,
   }
 });
+
 export async function apiPost(endpoint, data) {
   try {
-    let res = await request.post(endpoint, data);
-
+    let res = await request.post(endpoint, data)
     if (res) {
-      return res;
+      return res
     }
   } catch (error) {
     console.log('API POST ERROR endpoint:', endpoint, ' || error:', error);
   }
 }
+
 export const uploadImage = async (response, options) => {
   const BASE_URL = options.url;
   let data = new FormData();
@@ -130,4 +138,4 @@ export const uploadImage = async (response, options) => {
     data: response.data
   });
   let res = await apiPost(BASE_URL + '/modules/camera/upload_image/', data);
-};
+}
