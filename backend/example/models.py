f801724b6ed3c9a57e73ce5bb8a2166af28e25d6
crowdbotics-dev <engineering@crowdbotics.com>
2023-10-02 16:10:23 +0000

    Update models

--- backend/example/models.py
+++ backend/example/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Example(models.Model):
     'Generated Model'
     name = models.BigIntegerField()
-    rel_image_1_1 = models.OneToOneField("camera.Image",blank=True,null=True,on_delete=models.CASCADE,related_name="example_rel_image_1_1",)
+    rel_image_1_1 = models.OneToOneField("camera.Image",null=True,blank=True,on_delete=models.CASCADE,related_name="example_rel_image_1_1",)
 
 # Create your models here.
--- backend/example/views.py
+++ backend/example/views.py
@@ -1,3 +1,5 @@
+from .models import Example
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
 
 # Create your views here.
